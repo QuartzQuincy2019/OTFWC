6fe537a2bc3d4484fed7437ff3e8c0abeb638110
QuartzQuincy2019 <quincyk2019@163.com>
2026-02-12 21:23:33 +0800

    v2.0.0

--- js/display.js
+++ js/display.js
@@ -2,6 +2,9 @@
 const E_FontSelector = document.getElementById("FontSelector");
 const E_Checker = document.getElementById("Checker");
 const E_SizeButtons = document.getElementById("SizeButtons");
+const E_LetterButtons = document.getElementById("LetterButtons");
+
+var currentFontName = null;
 
 function initializeFontSelector() {
     E_FontSelector.innerHTML = "";
@@ -22,10 +25,11 @@ function initializeFontSelector() {
 initializeFontSelector();
 
 function updateCheckerFont() {
-    var selectedFontKey = E_FontSelector.value;
-    E_Checker.style.fontFamily = selectedFontKey;
-    var selectedLanguage = FONTS[selectedFontKey].classification.code;
-    E_Checker.placeholder = FONTS[selectedFontKey].sampleText;
+    currentFontName = E_FontSelector.value;
+    E_Checker.style.fontFamily = currentFontName;
+    var selectedLanguage = FONTS[currentFontName].classification.code;
+    currentLanguageCode = LANGUAGE_CLASSIFICATION[selectedLanguage].code;
+    E_Checker.placeholder = FONTS[currentFontName].sampleText;
     if (selectedLanguage === "ar") {
         E_Checker.classList.add("rtl");
     } else {
@@ -33,21 +37,51 @@ function updateCheckerFont() {
     }
 }
 
-E_FontSelector.addEventListener("change", function() {
+E_FontSelector.addEventListener("change", function () {
     updateCheckerFont();
+    toggleLetterButtons();
 });
 
 function createSizeButton(size) {
     const button = document.createElement("button");
     button.textContent = size + "px";
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
+        currentFontSize = size;
         E_Checker.style.fontSize = size + "px";
+        document.documentElement.style.setProperty("--target-button--font-size", currentFontSize + "px");
     });
     return button;
 }
 E_SizeButtons.innerHTML = "";
 const fontSizes = [16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512];
+var currentFontSize = null;
 fontSizes.forEach(size => {
     const button = createSizeButton(size);
     E_SizeButtons.appendChild(button);
 });
+
+function createLetterButton(letter) {
+    const button = document.createElement("button");
+    button.textContent = letter;
+    button.addEventListener("click", function () {
+        E_Checker.value += letter;
+    });
+    return button;
+}
+
+function toggleLetterButtons() {
+    var targetSet = null;
+    switch (currentLanguageCode) {
+        case "ar": targetSet = ArabicLetters; break;
+        case "sa": targetSet = DevanagariLetters; break;
+        case "km": targetSet = KhmerLetters; break;
+        default: E_LetterButtons.innerHTML = "";
+            break;
+    }
+    E_LetterButtons.innerHTML = "";
+    targetSet.forEach(letter => {
+        const button = createLetterButton(letter);
+        button.style.fontFamily = currentFontName;
+        E_LetterButtons.appendChild(button);
+    });
+}
